8c40cbbcf20d5c5db299d1bf44052bfc11f3db26
DESKTOP-DQ6JIAP\Vuvvi <valerio.ceraudo@gmail.com>
2018-02-10 04:19:29 +0100

    Project creation plus NetworkManager and player bidimensional movement

---
+++ DanceTovarish/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float fVelocityVertical;
+    public float fVelocityHorizontal;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float MovementVertical = 0;
+        float MovementHorizontal = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovementVertical = Time.deltaTime * fVelocityVertical;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MovementVertical = -Time.deltaTime * fVelocityVertical;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MovementHorizontal = Time.deltaTime * fVelocityHorizontal;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MovementHorizontal = -Time.deltaTime * fVelocityHorizontal;
+        }
+
+        transform.Translate(MovementHorizontal, MovementVertical, 0f);
+    }
+
+    
+}
