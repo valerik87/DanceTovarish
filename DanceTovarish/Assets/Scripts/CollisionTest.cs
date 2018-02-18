a987fa778d485ed36dd1775e459a6129bdbafa10
DESKTOP-DQ6JIAP\Vuvvi <valerio.ceraudo@gmail.com>
2018-02-18 20:37:55 +0100

    Bullet detection

--- DanceTovarish/Assets/Scripts/CollisionTest.cs
+++ DanceTovarish/Assets/Scripts/CollisionTest.cs
@@ -1,13 +1,12 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class CollisionTest : MonoBehaviour {
 
-    void OnCollisionEnter2D(Collision2D other)
+	void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("OnCollissionEnter" + gameObject.name);
-        Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D other)
